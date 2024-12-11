0ba8b4c6830d34c7674e167881d461480bd94d27
Prashanth N <imprash4u@gmail.com>
2024-12-12 01:38:25 +0530

    Doubly linked list insert and display implementation

--- src/DSAProblems/LinkedList/LinkedListMainClass.java
+++ src/DSAProblems/LinkedList/LinkedListMainClass.java
@@ -5,21 +5,26 @@ import org.w3c.dom.Node;
 // implementing a Linked list
 public class LinkedListMainClass {
     public static void main(String[] args) {
-        LList linkeList = new LList();
+     /*   LList linkeList = new LList();
         linkeList.insertFirstElement(2);
         linkeList.insertFirstElement(10);
         linkeList.insertFirstElement(8);
         linkeList.insertFirstElement(4);
         linkeList.insertLastElement(100);
-        linkeList.insert(10000,1);
+        linkeList.insert(10000, 1);
         linkeList.display();
         linkeList.deleteFirst();
         linkeList.display();
         linkeList.deleteLastElement();
         linkeList.display();
         linkeList.delete(0);
-        linkeList.display();
-      Node node = linkeList.find(8);
+        linkeList.display();*/
+
+        DoubleLList doubleLList = new DoubleLList();
+        doubleLList.insertFirst(100);
+        doubleLList.insertFirst(200);
+        doubleLList.insertFirst(300);
+        doubleLList.display();
 
     }
 }
