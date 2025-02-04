a18f3a6d468ad89431576206a04077e89b164510
Prashanth N <imprash4u@gmail.com>
2025-02-04 08:18:25 +0530

    Find Middle of the Linked List

--- src/DSAProblems/LinkedList/CycleInALinkedList.java
+++ src/DSAProblems/LinkedList/CycleInALinkedList.java
@@ -4,9 +4,27 @@ public class CycleInALinkedList {
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
-        head.next.next = head;
+        head.next.next = new ListNode(25);
+        head.next.next.next = head.next;
+     // do not print using while loop
+//        printLinkedList(head);
         System.out.println("isLoopPresent:" + findLLisLoop(head));
-        System.out.println("isLoopPresent:" + findLLisLoop(head));
+
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(2);
+        head2.next.next = new ListNode(25);
+        head2.next.next.next = new ListNode(15);
+        head2.next.next.next.next = new ListNode(20);
+        System.out.println("middle:" + findMiddleLoop(head2).value);
+    }
+
+    private static void printLinkedList(ListNode head) {
+        ListNode temp = head;
+//        while (temp != null){
+//            System.out.print(temp.value + " ");
+//            temp = temp.next;
+//        }
+
     }
 
     static boolean findLLisLoop(ListNode listNode) {
@@ -30,7 +48,6 @@ public class CycleInALinkedList {
         while (fast != null && fast.next != null ) {
             slow = slow.next;
             fast = fast.next.next;
-
         }
         return slow;
     }
