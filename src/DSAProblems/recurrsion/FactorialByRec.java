9ffcd7c8eab5b16487f8e28d20877d16535ce75b
Prashanth N <imprash4u@gmail.com>
2025-02-15 01:11:59 +0530

    Factorial Number using recursion

---
+++ src/DSAProblems/recurrsion/FactorialByRec.java
@@ -0,0 +1,16 @@
+package DSAProblems.recurrsion;
+
+public class FactorialByRec {
+    public static void main(String[] args) {
+        int number = 5;
+        int val = factorial(number);
+        System.out.println("factorial of 5 is:"+ val);
+    }
+
+    private static int factorial(int number) {
+        if (number == 0){
+            return 1;
+        }
+        return number * factorial(number - 1 );
+    }
+}
