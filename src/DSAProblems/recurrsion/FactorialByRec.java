a3a3ff8aebe0ddf6f55b73d6952043ff11e47093
Prashanth N <imprash4u@gmail.com>
2025-02-15 01:43:27 +0530

    Refactoring

--- src/DSAProblems/recurrsion/FactorialByRec.java
+++ src/DSAProblems/recurrsion/FactorialByRec.java
@@ -5,6 +5,9 @@ public class FactorialByRec {
         int number = 5;
         int val = factorial(number);
         System.out.println("factorial of 5 is:"+ val);
+        // sum of N
+        int sum = sum(number);
+        System.out.println("sum of n to 5 is:"+ sum);
     }
 
     private static int factorial(int number) {
@@ -12,5 +15,11 @@ public class FactorialByRec {
             return 1;
         }
         return number * factorial(number - 1 );
+    }
+    private static int sum(int number) {
+        if (number <= 1){
+            return 1;
+        }
+        return number + sum(number - 1 );
     }
 }
