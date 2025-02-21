2a5cee70501657991c10abeca51ba1370fac5f0e
Prashanth N <imprash4u@gmail.com>
2025-02-21 18:00:10 +0530

    Longest repeating char replacement frequency

--- src/DSAProblems/Strings/LongestRepeatatingCharReplacement.java
+++ src/DSAProblems/Strings/LongestRepeatatingCharReplacement.java
@@ -12,4 +12,32 @@ Input: s = "ABAB", k = 2
 Output: 4
 Explanation: Replace the two 'A's with two 'B's or vice versa.*/
 public class LongestRepeatatingCharReplacement {
+    public static void main(String[] args) {
+        String str = "AABABCCCBCC";
+        System.out.println("frequency:" + frequencyAfterCharReplacement(str, 2));
+    }
+
+    private static int frequencyAfterCharReplacement(String str, int i) {
+        int[] freq = new int[26];// define a array to store the frequency of each character;
+        int left = 0;
+        int maxFreq = 0;
+        int maxwindow = 0;
+        // now Start a loop till the end of the Array
+        for (int right = 0;right <  str.length();right++){
+            freq[str.charAt(right)- 'A']++;
+            // update max frequency of a char in the array
+            maxFreq = Math.max(maxFreq,freq[str.charAt(right) - 'A' ]);
+            int windowLength = right - left+1;
+
+            if(windowLength - maxFreq > i){
+                freq[str.charAt(left) - 'A']--;
+                left++;
+            }
+            windowLength = right - left + 1;
+            maxwindow = Math.max(maxwindow,windowLength);
+        }
+
+        return maxwindow;
+    }
+
 }
