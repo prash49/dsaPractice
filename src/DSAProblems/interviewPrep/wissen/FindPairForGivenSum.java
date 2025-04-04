ea6336597eedefedfe2e69d7757417d0fcd56e4c
Prashanth N <imprash4u@gmail.com>
2025-04-04 13:39:15 +0530

    Wissen Technology Practice

---
+++ src/DSAProblems/interviewPrep/wissen/FindPairForGivenSum.java
@@ -0,0 +1,26 @@
+package DSAProblems.interviewPrep.wissen;
+
+import java.util.*;
+
+public class FindPairForGivenSum {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 5, 7, -1);
+        int sum = 6;
+        System.out.println("pairs: " + findTheNumberOfPairs(list, sum));
+    }
+
+    private static int findTheNumberOfPairs(List<Integer> list, int sum) {
+        int pairs = 0;
+        Map<Integer, Integer> intCountMap = new HashMap<>();
+        for (Integer integer : list) {
+            int compliment = sum - integer;
+            if (intCountMap.containsKey(compliment)) {
+                pairs += intCountMap.get(compliment);
+            } else {
+                intCountMap.put(integer,intCountMap.getOrDefault(integer, 0)+1);
+            }
+            System.out.println(intCountMap);
+        }
+        return pairs;
+    }
+}
