efb1753ea4c37861f493faf9af9e6c78f854e652
Prashanth N <imprash4u@gmail.com>
2025-04-05 09:53:41 +0530

    Wissen Prep programs practice

---
+++ src/DSAProblems/interviewPrep/wissen/ProductOfArrayExceptItSelf.java
@@ -0,0 +1,31 @@
+package DSAProblems.interviewPrep.wissen;
+
+public class ProductOfArrayExceptItSelf {
+    public static void main(String[] args) {
+        int[] array = {1,0,2,4,5};
+        int[] productArray = productOfArrayExceptSelf(array);
+     }
+
+    private static int[] productOfArrayExceptSelf(int[] array) {
+        int product = 1;
+        int zeroCount = 0;
+        int[] productArray = new int[array.length];
+        for (int i = 0; i < array.length ; i++){
+            if (array[i] == 0){
+                zeroCount++;
+            }else { product *= array[i];}
+        }
+        for (int j =0; j < array.length; j++){
+            if (zeroCount ==1 && array[j] != 0){
+                productArray[j] = 0;
+            }else if (zeroCount ==1 && array[j] == 0){
+                productArray[j] = product;
+            }else if (zeroCount >= 2){
+                productArray[j] = 0;
+            }else {
+                productArray[j] = (product / array[j]);
+            }
+        }
+        return  productArray;
+     }
+}
---
+++ src/DSAProblems/interviewPrep/wissen/ReverseALinkedList.java
@@ -0,0 +1,36 @@
+package DSAProblems.interviewPrep.wissen;
+
+public class ReverseALinkedList {
+    public static void main(String[] args) {
+        ListNode2 head = new ListNode2(1);
+        head.next = new ListNode2(2);
+        head.next.next = new ListNode2(3);
+        ListNode2 reversedNode = reverseALinkedList(head);
+        while (reversedNode != null) {
+            System.out.println("value :" + reversedNode.value);
+            reversedNode = reversedNode.next;
+        }
+    }
+
+    static ListNode2 reverseALinkedList(ListNode2 head) {
+        ListNode2 prev = null, current = head;
+
+        while (current != null) {
+            ListNode2 temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+
+        }
+        return prev;
+    }
+}
+
+class ListNode2 {
+    int value;
+    ListNode2 next;
+
+    ListNode2(int value) {
+        this.value = value;
+    }
+}
