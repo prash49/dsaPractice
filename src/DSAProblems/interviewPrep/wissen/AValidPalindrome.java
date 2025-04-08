c5873a0e5b957699290a1ed6e9f1230323424f2f
Prashanth N <imprash4u@gmail.com>
2025-04-08 19:27:14 +0530

    Wissen Prep programs practice

---
+++ src/DSAProblems/interviewPrep/wissen/AValidPalindrome.java
@@ -0,0 +1,28 @@
+package DSAProblems.interviewPrep.wissen;
+
+public class AValidPalindrome {
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println("is it a Palindrome:"+ isItPalindrome(str));
+
+    }
+
+    private static boolean isItPalindrome(String str) {
+        // we are using two pointers to check
+        //hence initialize
+        int i = 0, j = str.length() -1;
+        while (i < j){
+            while (i < j && !Character.isLetterOrDigit(str.charAt(i))){
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(str.charAt(j))){
+                j--;
+            }
+            if (Character.toLowerCase(str.charAt(i)) != Character.toLowerCase(str.charAt(j))){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
