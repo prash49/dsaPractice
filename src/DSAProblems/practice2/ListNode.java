11c4225add42384c2b6c9fdbc4d50322ad396cda
Prashanth N <imprash4u@gmail.com>
2025-05-30 00:40:17 +0530

    Remove nth node from LinkedList

--- src/DSAProblems/practice2/ListNode.java
+++ src/DSAProblems/practice2/ListNode.java
@@ -3,8 +3,8 @@ package DSAProblems.practice2;
 
 public class ListNode {
     int value;
-    ListNode prev;
-    ListNode next;
+    public  ListNode prev;
+    public ListNode next;
 
     public ListNode(int value) {
         this.value = value;
